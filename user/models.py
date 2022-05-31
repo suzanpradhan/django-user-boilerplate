73ec4a43368025ca4d6a9c7b594ec5a5e03b85a0
Manish Kunwar <ranaxmond@gmail.com>
2022-05-31 17:51:05 +0545

    added __str__ and fullname for model

--- user/models.py
+++ user/models.py
@@ -9,3 +9,10 @@ class User(AbstractBaseUser):
     middle_name = models.CharField(max_length=64, null=True, blank=True)
     last_name = models.CharField(max_length=64, null=True, blank=True)
     phone = models.CharField(max_length=20,  null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.email
+    
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
